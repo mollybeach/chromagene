dda79d95c11c21cb9a712528e83ba06452d35dd2
mollybeach <56612434+mollybeach@users.noreply.github.com>
2021-03-31 22:41:19 -0700

    Cleaned up component

--- client/src/components/Home/Framie.jsx
+++ client/src/components/Home/Framie.jsx
@@ -1,30 +1,25 @@
-import React, { useEffect, useRef} from 'react';
-import {sculptToMinimalRenderer} from 'shader-park-core';
-import {spCode} from './spCode.js';
-
-const Framie = ({ref}) => {
-  const shadeRef= useRef(null);
+import { useEffect, useRef } from "react";
+import { sculptToMinimalRenderer } from "shader-park-core";
+import { spCode } from "./spCode.js";
+/****************USE HOOKS TO LOAD SHADER **********************/
+const Framie = ({ src }) => {
+  const shadeRef = useRef(null);
   useEffect(() => {
     if (shadeRef.current) {
-      const canvas = document.querySelector('.my-canvas');
+      const canvas = document.querySelector(".my-canvas");
       console.log(canvas);
-    sculptToMinimalRenderer(canvas, spCode);
+      sculptToMinimalRenderer(canvas, spCode);
     }
- }, [ref])
+  }, [src]);
 
-    return (
-      <div>
-         <canvas className="my-canvas" ></canvas>
-        <iframe id="bigfram" title='miframie' ref={shadeRef}
-     > </iframe>
-      </div>
+  return (
+    <div>
+      <canvas className="my-canvas"></canvas>
+      <iframe id="bigfram" title="miframie" ref={shadeRef}>
+        {" "}
+      </iframe>
+    </div>
+  );
+};
 
-    )
-  }
-
-  export default Framie;
-//style={{position: 'static', width: '300px', height: '300px'}}
-// style={{'height': '100%', 'width': '100%', 'border': '0'}}
-// style={{'height': '100%', 'width': '100%', 'border': '0'}}
-//src={sculptToMinimalRenderer(document.querySelector('.my-canvas'), spCode)}
-
+export default Framie;
