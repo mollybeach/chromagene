e6f0eb162c95a9b71ec9025fc7ce24af6be6875d
mollybeach <56612434+mollybeach@users.noreply.github.com>
2021-04-05 22:49:50 -0700

    Nice

--- client/src/components/UploadFile/UploadFile.jsx
+++ client/src/components/UploadFile/UploadFile.jsx
@@ -5,63 +5,58 @@ import './UploadFile.scss';
 import chevronImg from "../../assets/Icons/chevron_right-24px.svg";
 
 function FileUpload() {
-    const [file, storeFile] = useState(''); // storing the uploaded file    // storing the recived file from backend
-    const [data, getFile] = useState({ name: "", path: "" });    
-    const [progress, setProgess] = useState(0); // progess bar
-    const el = useRef(); // accesing input element
-    const changeEvent = (event) => {
-        setProgess(0)
-        const file = event.target.files[0]; 
+    /*******************STORE FILE WITH HOOKS****************************************/
+    let [data, grabFile] = useState({ name: "", path: "" });    
+    let [prog, set] = useState(0); 
+    let [file, storeFile] = useState(''); 
+    let el = useRef(); 
+    /**********************EVENT FUNCTION *********************************/
+    let onEvent = (event) => {
+        set(0)
+        let file = event.target.files[0]; 
         console.log(file);
         storeFile(file); 
     }
+       /**********************UPLOAD FUNCTION*********************************/
     const uploadFile = () => {
         const formData = new FormData();        
-        formData.append('file', file); // appending file
+        formData.append('file', file);
         axios.post(`${API_URL}/uploadfile`, formData, {
-            onUploadProgress: (ProgressEvent) => {
-                let progress = Math.round(
-                ProgressEvent.loaded / ProgressEvent.total * 100) + '%';
-                setProgess(progress);
+            onUploadProgress: (loadEvent) => {
+                let prog = Math.round(
+                loadEvent.loaded / loadEvent.total * 100) + '%';
+                set(prog);
             }
         }).then(res => {
             console.log(res);
-            getFile({ name: res.data.name,
+            grabFile({ name: res.data.name,
             path: { API_URL } + res.data.path
             })
         }).catch(err => console.log(err))}
-    return (
-        <>
-        <div className="upload">
-            <div className="hero-container "></div>
+        return (
+            <>
+            <div className="upload">
+                <div className="hero-container "></div>
+                <div className="content"></div>
+              <div className='upload__inside '>
+              <div className="upload__subtitle ">ChromaGene
+                <img className="upload__arrow" src={chevronImg} alt="img" />
+              </div>
+              <div className="upload__titleb ">Upload Your DNA file!</div>
+              <div className="upload__about " > Upload DNA .txt or .csv file to create unique art and begin your journey!
+                    <div className="upload__progessBar" style={{ width: prog }}>{prog}</div>
+                    <input type="file" ref={el} onChange={onEvent} id="mod-button" hidden/> 
+                    <label for="mod-button"  className="btn-form btn-form--delta" ><span>Choose File</span></label>   
+                    <button onClick={uploadFile} className="btn btn--gamma"><span>Upload</span></button>
+                     <hr />
+                    {data.path && <img src={data.path} alt={data.name} />}
+              </div>
+              </div>
+            </div>
+            <div className="content">
+            </div>
             <div className="content"></div>
-          <div className='upload__inside '>
-          <div className="upload__subtitle ">
-          ChromaGene
-            <img className="upload__arrow" src={chevronImg} alt="img" />
-          </div>
-    
-          <div className="upload__titleb ">Upload Your DNA file!</div>
-          <div className="upload__about " > Upload DNA .txt or .csv file to create unique art and begin your journey!
-                    
-                <div className="upload__progessBar" style={{ width: progress }}>{progress}</div>
-                <input type="file" ref={el} onChange={changeEvent} id="mod-button" hidden/> 
-                <label for="mod-button"  className="btn-form btn-form--delta" ><span>Choose File</span></label>   
-                <button onClick={uploadFile} className="btn btn--gamma"><span>Upload</span></button>
-                 <hr />
-                {data.path && <img src={data.path} alt={data.name} />}
-       
-          </div>
-          </div>
-          
-        </div>
-        <div className="content">
-    
-        </div>
-        <div className="content"></div>
-   </>
-
-     
-    );
-}
-export default FileUpload;
+       </>
+        );
+    }
+    export default FileUpload;
