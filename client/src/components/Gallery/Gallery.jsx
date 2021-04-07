9a1a727ae33bc3fcac6bc29a2e9d62fb5b06d2ca
mollybeach <mollyjbeach@gmail.com>
2021-04-07 00:06:37 -0700

    More stying

--- client/src/components/Gallery/Gallery.jsx
+++ client/src/components/Gallery/Gallery.jsx
@@ -206,9 +206,9 @@ axios.get('http://localhost:8080/gallery').then(res=>{
           <div className="gallery__title "> ChromaGene Genotype Display:
             <img className="gallery__arrow" src={chevronImg} alt="img" />
           </div>
-          <div className="gallery__title "> Each color represents one of the 16 SNPs
+          <div className="gallery__subtitle "> Each color represents one of the 16 SNPs
           </div>
-          <div className="gallery__subtitle "> Results Are Ready: </div>
+          <div className="gallery__subtitle "> Simulated Display: </div>
           <div className="gallery__about " >
           </div>
           </div>
