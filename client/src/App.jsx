92ef79cfe811b6e74137c3dc84c4e0e4e5b16ae2
mollybeach <mollyjbeach@gmail.com>
2021-08-06 18:04:52 -0700

    add heroku

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import Home from './components/Home/Home';
 import Gallery from './components/Gallery/Gallery';
 import Contact from "./components/Contact/Contact";
 import My23 from './components/My23/My23';
-//import SnpCodeLoad from './components/Gallery/snpCodeLoad';
+//import SnpCodeLoad from './components/Gallery/snpCodeLoadk';
 import UploadFile from "./components/UploadFile/UploadFile";
 //import Footer from './components/Footer/Footekkr';
 import './App.scss'
