c2baad915337242e513fa2428c086eb6c517ea8f
mollybeach <mollyjbeach@gmail.com>
2021-08-08 17:32:20 -0700

    Syntax error: Unexpected token (56:14)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -53,12 +53,13 @@ axios.get('http://localhost:8080/uploadfile').then(res=>{
   render() {
     const { homeList, my23List, galleryList, contactList, uploadFileList} = this.state;
     if(homeList ===null ) {  
-      return <> 
+      return (
+      <div> 
      <img className ="app__rainbows"src={company} alt=''></img>
        <div className = "app__load"></div>
        <div className = "app__text"> Thank you for waiting patiently. There's alot to load here! :) </div>
        <img className="app__ring" src={ring} alt=''></img>
-       </>
+       </div> )
        }
     return (
       <div className = 'app'>
