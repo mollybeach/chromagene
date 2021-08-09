df8eff7f82b3345a8279f8c2d6287e550c5fb2df
mollybeach <mollyjbeach@gmail.com>
2021-08-08 21:12:05 -0700

    or PORT statement

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 8080;
+const PORT = process.env.PORT ||8080;
 const path = require('path');
 const cors = require("cors");
 const home = require("./routes/home");
